4deab4f7ece352949de2d4ff4620bc8843396342
Yunseok Kang <mok02198@gmail.com>
2022-11-18 23:08:20 +0900

    Create 기사단원의 무기.py

---
+++ 연습문제/기사단원의 무기.py
@@ -0,0 +1,23 @@
+import math
+
+def getNum(n):
+    cnt = 0
+    for i in range(1, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            cnt += 1
+            if i * i < n:
+                cnt += 1
+    return cnt
+
+def solution(number, limit, power):
+    answer = 0
+
+    for i in range(1, number + 1):
+        result = getNum(i)
+
+        if result <= limit:
+            answer += result
+        else:
+            answer += power
+
+    return answer

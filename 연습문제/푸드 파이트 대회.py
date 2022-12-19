ddad6640d364316fb783e5550dc3158cf44b0643
Yunseok Kang <mok02198@gmail.com>
2022-12-19 23:52:30 +0900

    Update 푸드 파이트 대회.py

--- 연습문제/푸드 파이트 대회.py
+++ 연습문제/푸드 파이트 대회.py
@@ -1,9 +1,7 @@
 from itertools import chain
 def solution(food):
     stack = []
-
     for i in range(1, len(food)):
         for _ in range(food[i] // 2):
             stack.append(i)
-
     return ''.join(map(str, chain(stack, [0], stack[::-1])))
